4cb99f35a15608c471ad7a8dca655377d89d06ca
knevo <nevok18@gmail.com>
2020-02-07 17:55:10 +0200

    update

--- js/projService.js
+++ js/projService.js
@@ -18,20 +18,35 @@ function createProj(id, name, title, desc, url, publishedAt, category) {
 }
 
 function createProjects() {
+    gProj.push(createProj('webify', 'Webify',
+        'Create powerful responsive websites', 'A simple drag & drop website builder with the power to create modern and responsive single page websites in minutes', 'webify-ca.herokuapp.com', '28/1/20', 'React, Redux, Wix, APIs'))
+
+    gProj.push(createProj('appsus', 'Appsus',
+        '3 Apps in one place', 'Powerfull app that includes 3 different applications inside- a Gmail like app, a notes app and a book store management app', 'http://appsus.herokuapp.com/', '23/12/19', 'React, APIs'))
+
+    gProj.push(createProj('meme-gen', 'Meme generator', 'Memes', 'A site to create and share custom memes', 'assets/projects/meme-gen/index.html', '18/12/19', 'Canvas'))
+
     gProj.push(createProj('minesweeper', 'Mine Sweeper',
         'sweep those mines', 'a game', 'assets/projects/mine-sweeper/index.html', '23/11/19', 'Games'))
+
     gProj.push(createProj('bookshop', 'Book Shop',
         'Book shop manager', 'great interface for library managers', 'assets/projects/book-store/index.html', '1/12/19', 'Interface'))
+
     gProj.push(createProj('safe-content', 'Safe content',
         'Secret content inside', 'its a secret', 'assets/projects/safe-content/index.html', '25/11/19', 'Interface'))
+
     gProj.push(createProj('todos', 'Todos',
         'Todo List', 'Manage your todos', 'assets/projects/todos/index.html', '25/11/19', 'Interface'))
+
     gProj.push(createProj('color-guess', 'Color game', 'Guess the color', 'Guess which color the RGB stands for', 'assets/projects/color-guess/index.html', '8/4/19'))
+
     gProj.push(createProj('guess-me', 'Guess Game', 'Akinator', 'Think of any character and i will guess it', 'assets/projects/guess-me/index.html', '24/11/19', 'Tree traverse'))
+
     gProj.push(createProj('mister-blog', 'Blog template', 'News blog', 'A template for any news or blog site', 'assets/projects/misterblog/Site/index.html', '8/12/19', 'HTML Template'))
-    gProj.push(createProj('meme-gen', 'Meme generator', 'Memes', 'A site to create and share costum memes', 'assets/projects/meme-gen/index.html', '18/12/19', 'Canvas'))
-    gProj.push(createProj('travel-tip', 'Google API', 'API', 'Pin-Point locations on the map', 'assets/projects/travel-tip/index.html', '23/12/19', 'Google API,API'))
-    gProj.push(createProj('wiki-tube', 'Youtube API', 'API', 'Search and play youtube videos', 'assets/projects/wiki-tube/index.html', '23/12/19', 'Youtube API,API,wikipedia'))
+
+    gProj.push(createProj('travel-tip', 'Google API', 'API', 'Pin-Point locations on the map', 'assets/projects/travel-tip/index.html', '23/12/19', 'Google API, API'))
+
+    gProj.push(createProj('wiki-tube', 'Youtube API', 'API', 'Search and play youtube videos', 'assets/projects/wiki-tube/index.html', '23/12/19', 'Youtube API, API, wikipedia'))
 }
 
 function getProjectsToRender() {
